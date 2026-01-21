bb91ba055c8a3552fc6cd66d41e48dcc5b4afdb4
Miguel <91156124+ImMigs@users.noreply.github.com>
2026-01-21 01:20:33 -0300

    Delete assets directory

--- assets/js/homepage/homepage.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const changelog = document.querySelector('.container-changelog');
-    if (changelog) {
-        const header = changelog.querySelector('.changelog-header');
-        header.addEventListener('click', () => {
-            changelog.classList.toggle('active');
-        });
-    }
-});
